aeac4ae94bd36ded414ac3a1a31f0bf220f25bff
Marc Jones <m4rcj0n35@gmail.com>
2022-11-06 21:53:32 +0000

    Basic player controller

---
+++ scripts/Mirror.gd
@@ -0,0 +1,7 @@
+extends StaticBody2D
+
+const shiny = true
+
+func _ready():
+	pass # Replace with function body.
+	
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,17 +1,23 @@
 extends KinematicBody2D
 
-const speed = 50.0
+const WALK_FORCE = 600
+const WALK_MAX_SPEED = 200
+const STOP_FORCE = 1300
+const JUMP_SPEED = 200
 const shiny = true
 
-func _ready():
-	pass # Replace with function body.
+var velocity = Vector2()
+
+onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_left"):
-		position.x -= speed * delta
-	if Input.is_action_pressed("ui_right"):
-		position.x += speed * delta
-	if Input.is_action_pressed("ui_up"):
-		position.y -= speed * delta
-	if Input.is_action_pressed("ui_down"):
-		position.y += speed * delta
+	var walk = WALK_FORCE * (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
+	if abs(walk) < WALK_FORCE * 0.2:
+		velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
+	else:
+		velocity.x += walk * delta
+	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
+	velocity.y += gravity * delta
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = -JUMP_SPEED
